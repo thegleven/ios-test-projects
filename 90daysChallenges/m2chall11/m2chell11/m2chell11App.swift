d1bbad9bc7ebec9065d9d63a4c6ab915800ea3da
thegleven <thegleven@gmail.com>
2024-03-01 17:39:55 +0000

    add challenge 11 module 2 from 90days course

---
+++ 90daysChallenges/m2chall11/m2chell11/m2chell11App.swift
@@ -0,0 +1,17 @@
+//
+//  m2chell11App.swift
+//  m2chell11
+//
+//  Created by theshamuel on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct m2chell11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
