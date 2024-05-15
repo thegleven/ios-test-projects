aa14159b2445fa27a7d8bf401ad6ad52a6b7707f
thegleven <thegleven@gmail.com>
2024-05-15 19:54:16 +0100

    add ContentDetailView for app MyLearning, modul 5

---
+++ 90daysChallenges/MyLearning/MyLearning/Data/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  MyLearning
+//
+//  Created on 15/05/2024.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
