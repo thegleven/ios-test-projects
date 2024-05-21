404993a2a8fbd779ed7fb3900583744401d74f45
thegleven <thegleven@gmail.com>
2024-05-21 12:05:21 +0100

    add app that plays a video, lesson 6 challenge, module 5

---
+++ 90daysChallenges/m5chall6/m5chall6/m5chall6App.swift
@@ -0,0 +1,17 @@
+//
+//  m5chall6App.swift
+//  m5chall6
+//
+//  Created on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct m5chall6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
