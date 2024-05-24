4a5e5717bbf0df49b55f95d66ab695f70e2c223e
thegleven <thegleven@gmail.com>
2024-05-24 12:08:44 +0100

    add CodeTextView, use UIKit, add style.html, lesson 7, module 5

---
+++ 90daysChallenges/MyLearning/MyLearning/Views/CodeTextView.swift
@@ -0,0 +1,35 @@
+//
+//  CodeTextView.swift
+//  MyLearning
+//
+//  Created on 24/05/2024.
+//
+
+import SwiftUI
+
+struct CodeTextView: UIViewRepresentable {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    func makeUIView(context: Context) -> UITextView {
+        
+        let textView = UITextView()
+        
+        textView.isEditable = false
+        
+        return textView
+    }
+    
+    func updateUIView(_ textView: UITextView, context: Context) {
+        
+        //set the attributed text for the lesson
+        textView.attributedText = model.lessonDescription
+        
+        //scroll back to the top
+        textView.scrollRectToVisible(CGRect(x: 0, y: 0, width: 1, height: 1), animated: false)
+    }
+}
+
+#Preview {
+    CodeTextView()
+}
