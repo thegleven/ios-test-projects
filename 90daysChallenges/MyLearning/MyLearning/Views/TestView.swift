02ca9a62cc7c989cb52a75b0c944a244159cbbed
thegleven <thegleven@gmail.com>
2024-05-30 17:09:28 +0100

    add submit button in TestView, highlight appropriate colors, app MyLearning, module 5

--- 90daysChallenges/MyLearning/MyLearning/Views/TestView.swift
+++ 90daysChallenges/MyLearning/MyLearning/Views/TestView.swift
@@ -10,23 +10,101 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex: Int?
+    @State var submitted = false
+    @State var numCorrect = 0
     
     var body: some View {
         
         if model.currentQuestion != nil {
             
-            VStack {
+            VStack(alignment: .leading) {
                 
                 //question number
                 Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
                 
                 //question
                 CodeTextView()
+                    .padding(.horizontal, 20)
                 
                 //answers
+                ScrollView {
+                    VStack {
+                        ForEach (0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            
+                            Button {
+                                
+                                selectedAnswerIndex = index
+                                
+                            } label: {
+                                
+                                ZStack {
+                                    
+                                    if submitted == false {
+                                        
+                                        RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                            .frame(height: 48)
+                                        
+                                    } else {
+                                        
+                                        if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex {
+                                            
+                                            RectangleCard(color: Color.green)
+                                                .frame(height: 48)
+                                            
+                                        } else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                            
+                                            RectangleCard(color: Color.red)
+                                                .frame(height: 48)
+                                            
+                                        } else if index == model.currentQuestion!.correctIndex {
+                                            
+                                            RectangleCard(color: Color.green)
+                                                .frame(height: 48)
+                                            
+                                        } else {
+                                            
+                                            RectangleCard(color: Color.white)
+                                                .frame(height: 48)
+                                        }
+                                    }
+                                    
+                                    Text(model.currentQuestion!.answers[index])
+                                }
+                            }
+                            .disabled(submitted)
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
+                }
                 
                 //button
                 
+                Button {
+                    
+                    submitted = true
+                    
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex {
+                        
+                        numCorrect += 1
+                    }
+                    
+                } label: {
+                    
+                    ZStack {
+                        
+                        RectangleCard(color: .green)
+                            .frame(height: 48)
+                        
+                        Text("Submit")
+                            .foregroundColor(.white)
+                            .bold()
+                    }
+                    .padding()
+                }
+                .disabled(selectedAnswerIndex == nil)
             }
             .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
         }
@@ -37,6 +115,6 @@ struct TestView: View {
     }
 }
 
-#Preview {
-    TestView()
-}
+//#Preview {
+//    TestView()
+//}
