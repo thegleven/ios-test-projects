e701055d22ba581c08a43338c271c6af51f85238
thegleven <thegleven@gmail.com>
2024-06-10 12:47:34 +0100

    add app that retrieves facts from numbersapi.com and display them to the user, module 5, challenge 14

---
+++ 90daysChallenges/m5chall14/m5chall14/m5chall14App.swift
@@ -0,0 +1,17 @@
+//
+//  m5chall14App.swift
+//  m5chall14
+//
+//  Created on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct m5chall14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(model: Model())
+        }
+    }
+}
