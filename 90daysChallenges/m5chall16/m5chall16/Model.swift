c796bb205db2a1aef92d5ec0448cb57b0277b090
thegleven <thegleven@gmail.com>
2024-06-11 13:10:52 +0100

    add model, func getData app allVideo, challenge16, module 5

---
+++ 90daysChallenges/m5chall16/m5chall16/Model.swift
@@ -0,0 +1,42 @@
+//
+//  Model.swift
+//  m5chall16
+//
+//  Created on 11/06/2024.
+//
+
+import Foundation
+
+class Model: ObservableObject {
+    
+    var allVideo = [Video]()
+    @Published var videos = [Video]()
+    
+    init() {
+        
+        self.getData()
+    }
+    
+    func getData() {
+        
+        guard let url = URL(string: "https://codewithchris.github.io/Module5Challenge/Data.json")
+        else {
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            if let data = data, let videos = try? JSONDecoder().decode([Video].self, from: data) {
+                
+                self.allVideo = videos
+                
+                DispatchQueue.main.async {
+                    
+                    self.videos = videos
+                }
+            }
+            
+        }
+        .resume()
+    }
+}
